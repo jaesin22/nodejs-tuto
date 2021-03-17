46859233ca84f06b335e9a420b0cb4cfed8e6bf6
jaesin22 <jaesin22@naver.com>
2021-03-17 15:18:06 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key')
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jaesin22:rktepa2@@node.qf8kc.mongodb.net/node?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('hello world'))
+
+app.post('/register', (req, res) =>  {
+
+
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({suceess: false, err})
+        return res.status(200).json({
+            suceess:true
+        })
+    })
+});
 
 app.listen(port, () => console.log('example app listening on port ${port}!'))
