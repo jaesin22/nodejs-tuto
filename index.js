ec94d540c0c2250d73ae980fc64cf45c1b924427
jaesin22 <jaesin22@naver.com>
2021-03-22 16:37:10 +0900

    03-22 로그인 기능 추가 후 commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express()
-const port = 3000
+const port = 5000
 
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
 const config = require('./config/key')
+const cookieParser = require('cookie-parser');
 
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 
@@ -31,7 +33,36 @@ app.post('/register', (req, res) =>  {
         return res.status(200).json({
             suceess:true
         })
+        console.log(doc);
     })
-});
+}); 
 
-app.listen(port, () => console.log('example app listening on port ${port}!'))
+app.post('/login', (req,res) => {
+    //요청된 이메일을 db에서 읽는지 찾는다
+    User.findOne({ email: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+            return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+        
+            //비밀번호까지 맞다면 토큰을 생성하기
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                // 토큰을 저장한다(쿠키에)
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+})
+
+app.listen(port, () => console.log(`example app listening on port ${port}!`))
