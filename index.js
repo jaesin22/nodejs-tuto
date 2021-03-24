8dacae9ca3c333072053a1feb6b5aa9098fec58b
jaesin22 <jaesin22@naver.com>
2021-03-24 18:06:10 +0900

    03-24 로그아웃 에러 미 조치후 commit_kimhj - 추후 조치 예정

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 5000
 
 const bodyParser = require('body-parser');
+
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User');
 const config = require('./config/key')
 const cookieParser = require('cookie-parser');
@@ -23,7 +25,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('hello world'))
 
-app.post('/register', (req, res) =>  {
+app.post('/api/users/register', (req, res) =>  {
 
 
     const user = new User(req.body)
@@ -37,7 +39,7 @@ app.post('/register', (req, res) =>  {
     })
 }); 
 
-app.post('/login', (req,res) => {
+app.post('/api/users/login', (req,res) => {
     //요청된 이메일을 db에서 읽는지 찾는다
     User.findOne({ email: req.body.email}, (err, user) => {
         if(!user) {
@@ -64,5 +66,34 @@ app.post('/login', (req,res) => {
         })
     })
 })
+// role 0 -> 일반 유저, role 0이 아니면 관리자
+app.get('/api/users/auth', auth, (req, res) => {
+
+    //여기까지 미들웨어를 통과했다는 얘기는 authentication 이 true라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name : req.user.name,
+        lastname : req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+
+    })
+})
+
+app.get('api/users/logout', auth, (req, res) => {
+    console.log('req.user', req.user)
+    User.findOneAndUpdate({ _id : req.user._id },
+        { token: "" }
+        , (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
+
 
 app.listen(port, () => console.log(`example app listening on port ${port}!`))
