d5d5bced7899f10d2d08c1a49064bef5de067fea
jaesin22 <jaesin22@naver.com>
2021-03-26 13:31:44 +0900

    로그아웃 처리 수정 완료

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get('/api/users/auth', auth, (req, res) => {
     })
 })
 
-app.get('api/users/logout', auth, (req, res) => {
+app.get('/api/users/logout', auth, (req, res) => {
     console.log('req.user', req.user)
     User.findOneAndUpdate({ _id : req.user._id },
         { token: "" }
