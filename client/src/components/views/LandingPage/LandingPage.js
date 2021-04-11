e5c92c112be406836d00b71933d0a78bcd7e606d
jaesin22 <jaesin22@naver.com>
2021-04-11 23:02:55 +0900

    로그인 처리 끝

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,9 +8,11 @@ function LandingPage() {
         .then(response => {console.log(response)})
     }, [])
     return (
-        <div>
-            LandingPage
-            checkout
+        <div style ={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+            }}>
+            <h2>시작 페이지</h2>
         </div>
     )
 }
