0cb5a11a290f76be2ce3bbaf75c8498dad670b39
jaesin22 <jaesin22@naver.com>
2021-04-13 00:07:28 +0900

    회원가입 기능 구현 중 커밋

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,4 +1,3 @@
-import Axios from 'axios'
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { loginUser } from '../../../_actions/user_action'
@@ -31,7 +30,7 @@ function LoginPage(props) {
             if(response.payload.loginSuccess) {
                 props.history.push('/')
             } else {
-                alert('Error')
+               // alert('Error')
             }
         })
 
