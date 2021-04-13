364d4e475a8aff7edc11cca91e3d43cc269d2709
jaesin22 <jaesin22@naver.com>
2021-04-13 22:20:36 +0900

    post register 부분 success 오탈자 수정

--- server/index.js
+++ server/index.js
@@ -29,15 +29,16 @@ app.get('/api/hello', (req, res) => res.send('Hello world'))
 
 app.post('/api/users/register', (req, res) =>  {
 
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
 
     const user = new User(req.body)
 
-    user.save((err, doc) => {
+    user.save((err, userInfo) => {
         if(err) return res.json({suceess: false, err})
         return res.status(200).json({
-            suceess:true
+            success:true
         })
-        console.log(doc);
     })
 }); 
 
