596cd19f68534021126f965ab70149a5c448eeb4
jaesin22 <jaesin22@naver.com>
2021-04-16 22:29:34 +0900

    Auth 처리 완료

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,3 +1,4 @@
+import { PresetColorTypes } from 'antd/lib/_util/colors';
 import Axios from 'axios';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
@@ -14,9 +15,26 @@ export default function(SpecificComponent, option, adminRoute = null) {
         const dispatch = useDispatch();
 
         useEffect(() => {
-
             dispatch(auth()).then(response => {
                 console.log(response);
+
+                //로그인 하지 않은 상태
+                if(!response.payload.isAuth) {
+                    if(option) {
+                        props.history.push('/login')
+                    }
+                    
+                } else {
+                    //로그인한 상태
+                    if(adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/')
+
+                    } else {
+                        if(option === false) {
+                            props.history.push('/')
+                        }
+                    }
+                }
             })
 
         }, [])
